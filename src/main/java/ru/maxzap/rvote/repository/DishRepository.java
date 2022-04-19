72d1017a874e27d68be9aebd59e84a3198255293
xdevil013 <xdevil013@gmail.com>
2022-04-19 14:24:12 +0300

    refact sql scripts

--- src/main/java/ru/maxzap/rvote/repository/DishRepository.java
+++ src/main/java/ru/maxzap/rvote/repository/DishRepository.java
@@ -2,7 +2,8 @@ package ru.maxzap.rvote.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.maxzap.rvote.model.Dish;
 
 @Transactional(readOnly = true)
-public interface DishRepository extends JpaRepository {
+public interface DishRepository extends JpaRepository<Dish, Integer> {
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,68 +1,9 @@
-DROP TABLE IF EXISTS users;
-DROP SEQUENCE IF EXISTS global_seq;
+Insert Into users (name, email, password)
+values ('admin', 'admin@gmail.com', 'admin'),
+       ('user', 'user@gmail.com', 'user'),
+       ('guest', 'guest@gmail.com', 'guest');
 
-/*CREATE SEQUENCE global_seq start with 1000;*/
-
-CREATE TABLE users
-(
---     //ToDo create Global sequens
-    /*id               integer not null PRIMARY KEY default nextval('global_seq'),*/
-    id         bigint auto_increment PRIMARY KEY,
-    name       VARCHAR                 NOT NULL,
-    email      VARCHAR                 NOT NULL,
-    password   VARCHAR                 NOT NULL,
---     //ToDo registered need?
---     registered TIMESTAMP DEFAULT now() NOT NULL,
-    enabled    BOOL      DEFAULT TRUE  NOT NULL
-);
-
-CREATE TABLE user_roles
-(
-    user_id INTEGER      NOT NULL,
-    role    VARCHAR(255) NOT NULL,
-    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
-    FOREIGN KEY (user_id) REFERENCES USERS (id) ON DELETE CASCADE
-);
-
-CREATE TABLE dish
-(
-    id    INTEGER auto_increment PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
---     //ToDo select data type for price
-    price double       NOT NULL
-);
-
-CREATE TABLE restaurant
-(
-    id      INTEGER auto_increment PRIMARY KEY,
-    name    VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL
-);
-
--- Menu
--- ---------------------------
--- id | date | restaraunt_id |
-CREATE TABLE menu
-(
-    id      INTEGER auto_increment PRIMARY KEY,
-    date    date    NOT NULL,
-    restaurant_id INTEGER NOT NULL,
-    FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE
-);
-
--- Voice
--- -------------------------------
--- id | date | user_id | menu_id |
-CREATE TABLE voice
-(
-    id       INTEGER auto_increment PRIMARY KEY,
-    dateTime TIMESTAMP NOT NULL,
-    user_id  INTEGER   NOT NULL,
-    menu_id  INTEGER   NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE
-);
-
--- Menu_Dish
--- -----------------
--- id_menu | id_dish
+/*Insert Into user_roles (user_id, role)
+values ('admin', 'admin@gmail.com', 'admin'),
+       ('user', 'user@gmail.com', 'user'),
+       ('guest', 'guest@gmail.com', 'guest');*/
